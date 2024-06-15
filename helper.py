e9c763a721c67162e0c9761d7fac6570e3ce4f69
Rithin Chalumuri <rithin.chalumuri@reewild.com>
2024-06-15 19:58:01 +0100

    test

--- helper.py
+++ helper.py
@@ -9,10 +9,7 @@ import os
 locale.setlocale(locale.LC_ALL, '')
 
 load_dotenv()
-api_key = os.environ.get("OPENAI_API_KEY")
-
-if not api_key:
-    api_key = st.secrets["OPENAI_API_KEY"]
+api_key = st.secrets["OPENAI_API_KEY"]
 
 #base_api_url = "http://localhost:7071"
 
